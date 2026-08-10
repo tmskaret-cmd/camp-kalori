9b72205cd282644042d52c4be4fe2bf7c6d54159
tmskaret-cmd <tmskaret@gmail.com>
2026-08-10 13:39:15 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+/* Camp Kalori service worker – cache-first, slik at appen virker uten nett */
+const CACHE = 'campkalori-v5';
+const FILES = ['./', './index.html', './manifest.json', './ikon-180.png', './ikon-192.png', './ikon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(hit => {
+      const net = fetch(e.request).then(res => {
+        if (res && res.ok) {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => hit);
+      return hit || net;
+    })
+  );
+});
